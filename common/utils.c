c8265be72d57f67ee68c325d60217fd45f571558
Priyesh Padmavilasom <ppadmavilasom@vmware.com>
2017-09-26 20:24:55 +0000

    fix cli param handling, allow command line overrides for url,user,pass, fix cmd search
    Change-Id: I20e7809e92a3c919721e2e2b59584e4da774a7fe

--- common/utils.c
+++ common/utils.c
@@ -173,3 +173,64 @@ error:
     SAFE_FREE_MEMORY(pszResult);
     goto cleanup;
 }
+
+uint32_t
+read_password_no_echo(
+    char **ppszPassword
+    )
+{
+    uint32_t dwError = 0;
+    char pszPasswordBuff[100] = {0};
+    char *pszPassword = NULL;
+    struct termios tp = {0};
+    struct termios save = {0};
+    int nLength = 0;
+
+    if(!ppszPassword)
+    {
+        dwError = EINVAL;
+        BAIL_ON_ERROR(dwError);
+    }
+
+    fflush(stdout);
+
+    tcgetattr(0, &tp) ;
+    memcpy (&save, &tp, sizeof (struct termios));
+    save.c_lflag &= ~ECHO;                /* ECHO off, other bits unchanged */
+    tcsetattr(0, TCSANOW, &save);
+    if (!fgets(pszPasswordBuff, 100, stdin) && ferror(stdin))
+    {
+        dwError = ferror(stdin);
+        BAIL_ON_ERROR (dwError);
+    }
+
+    nLength = strlen(pszPasswordBuff);
+    if(nLength <= 0)
+    {
+        dwError = EINVAL;
+        BAIL_ON_ERROR(dwError);
+    }
+
+    if (pszPasswordBuff[nLength - 1] == '\n')
+    {
+        pszPasswordBuff[nLength - 1] = '\0';
+    }
+
+    dwError = coapi_allocate_string(pszPasswordBuff, &pszPassword);
+    BAIL_ON_ERROR (dwError);
+
+    *ppszPassword = pszPassword;
+
+cleanup:
+    tcsetattr(0, TCSANOW, &tp);
+    fflush (stdin);
+    return dwError;
+
+error:
+    if (ppszPassword)
+    {
+        *ppszPassword = NULL;
+    }
+    SAFE_FREE_MEMORY(pszPassword);
+    goto cleanup;
+}
